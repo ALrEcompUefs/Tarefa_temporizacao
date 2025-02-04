98a9c43f5f14dff581c1d31fbe3d02e133278e14
ALrEcompUefs <contaofcali@gmail.com>
2025-02-04 18:18:42 -0300

    Update temporizador_por_disparo.c

--- temporizador_por_disparo/temporizador_por_disparo.c
+++ temporizador_por_disparo/temporizador_por_disparo.c
@@ -14,7 +14,7 @@ static uint contador;       //contador de repetições do alarme
 static bool ativar_timer;  // flag de controle da ativação do timer
 static bool timer_ativo=false;  // flag de controle da ativação do timer
 // Variavél para registro de tempo e controle de bounce da interrupção
-static volatile uint32_t tempo_anterior = 0,tempo_anterior2=0; 
+static volatile uint32_t tempo_anterior = 0;
 
 // protótipos de funções
 void inicializar_leds();
@@ -42,17 +42,20 @@ int main()
         printf("Flags\n");
         printf("Botao:%d\nalarme:%d\nContador:%d\n",ativar_timer,timer_ativo,contador);
         printf("------------------------------------------\n");
+        // libera ativação de um alrme se o botão foi pressionado e não está com um alarme ativo
         if(ativar_timer && !timer_ativo){
+            // atualiza flag do alarme
             timer_ativo=true;
             // limpa flag do botão
             ativar_timer = false;
-            // seta contador
+            // seta contador em 2 para que ocorram 3 execuções
             contador=2;
             // ativa todos os leds
             set_rgb('W');
             // configura alarme para 3s
             add_alarm_in_ms(3000, turn_off_callback, NULL, false);
         }
+        // delay para exibição das flags
         sleep_ms(1000);
     }
 }
@@ -131,6 +134,11 @@ void inicializar_botoes(){
     gpio_pull_up(BOTAO_B);
 }
 
+/*
+|   Função de callback do tratamento de interrupções
+|   Implementa um deboucer utilizando temporizador de sistema.
+|   Faz o tratamento das interrupções dos botões A e B
+*/
 static void gpio_irq_handler(uint gpio, uint32_t events){
     // obtém tempo absoluto do instante atual
     uint32_t tempo_atual = to_us_since_boot(get_absolute_time());
@@ -147,38 +155,43 @@ static void gpio_irq_handler(uint gpio, uint32_t events){
     }
 }
 
-// Função de callback para desligar o LED após o tempo programado.
+/* 
+|   Função de callback para desligar o LED após o tempo programado.
+|   O retorno da função define se o alarme vai rearmado ou não
+|   Se o retorno for 0 o alarme não é rearmado
+|   Com um retorno em valor maior que zero, o alarme é rearmado com o tempo
+|   passado pelo retorno
+|*/
 int64_t turn_off_callback(alarm_id_t id, void *user_data) {
-    uint32_t tempo_atual = to_us_since_boot(get_absolute_time());
-    // variavel para controle da repetição do alarme
+    // Valor padrão para retorno da função
+    // faz com que um novo alarme seja acionado em 3 segundos
     int64_t repetir = 3000000;
+
     // veririfca qual a proxima condição do contador
-    
     switch (contador)
     {
     case 2:
         // com o contador em 2, apaga uma led e deixa duas acesas
         printf("Primeiro alarme\n");
         contador--;
-        printf("Contador em:%d",contador);
-        printf("Tempo passado:%d\n",tempo_anterior2-tempo_atual);
+        //printf("Contador em:%d",contador);
         set_rgb('Y');
         break;
     case 1:
         // com o contador em 1, apaga uma led e deixa uma acesas
         printf("Segundo alarme alarme\n");
         contador--;
-        printf("Contador em:%d\n",contador);
-        printf("Tempo passado:%d\n",tempo_anterior2-tempo_atual);
+        //printf("Contador em:%d\n",contador);
         set_rgb('R');
         break;
     case 0:
         printf("Ultimo alarme\n");
-        printf("Contador em:%d\n",contador);
-        printf("Tempo passado:%d\n",tempo_anterior2-tempo_atual);
+        //printf("Contador em:%d\n",contador);
+
         // com o contador em 0, apaga todas as leds e encerra a repetição do alarme
         set_rgb('-');
         repetir= 0;
+        // atualiza flag do alarme liberando para novas ativações
         timer_ativo= false;
         break;
     default:
